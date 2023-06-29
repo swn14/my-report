f075c7523cacd4a0379b5fc6bf0bc33e7e1923f6
Sheldon Nofer <snofer@bethel.jw.org>
2023-06-29 15:16:29 -0400

    feat: save data to indexedDb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private storage: StorageMap) {}
   title = 'my-report';
+
+  onSubmit(
+    value: Partial<{
+      year: number | null;
+      month: string | null;
+      hours: number | null;
+      placements: number | null;
+      videoShowings: number | null;
+      returnVisits: number | null;
+      bibleStudies: number | null;
+    }>
+  ) {
+    console.log(value);
+    console.log('submit event happened!');
+    this.storage.set(`${value.year}-${value.month}`, value).subscribe();
+  }
 }
--- src/app/report-form/report-form.component.ts
+++ src/app/report-form/report-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 
 import { FormBuilder, Validators } from '@angular/forms';
 
@@ -8,8 +8,32 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./report-form.component.scss'],
 })
 export class ReportFormComponent {
+  @Output() public onSubmitEvent = new EventEmitter();
   private fb = inject(FormBuilder);
-  addressForm = this.fb.group({
+  currentDate = new Date();
+  years = this.range(
+    this.currentDate.getFullYear() - 1,
+    this.currentDate.getFullYear() + 10
+  );
+  currentYear = this.currentDate.getFullYear();
+  currentMonth = this.currentDate.getMonth();
+  months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  myReportForm = this.fb.group({
+    year: [this.currentYear, Validators.required],
+    month: [this.months[this.currentMonth], Validators.required],
     hours: [0, Validators.required],
     placements: 0,
     videoShowings: 0,
@@ -17,71 +41,14 @@ export class ReportFormComponent {
     bibleStudies: 0,
   });
 
-  hasUnitNumber = false;
-
-  states = [
-    { name: 'Alabama', abbreviation: 'AL' },
-    { name: 'Alaska', abbreviation: 'AK' },
-    { name: 'American Samoa', abbreviation: 'AS' },
-    { name: 'Arizona', abbreviation: 'AZ' },
-    { name: 'Arkansas', abbreviation: 'AR' },
-    { name: 'California', abbreviation: 'CA' },
-    { name: 'Colorado', abbreviation: 'CO' },
-    { name: 'Connecticut', abbreviation: 'CT' },
-    { name: 'Delaware', abbreviation: 'DE' },
-    { name: 'District Of Columbia', abbreviation: 'DC' },
-    { name: 'Federated States Of Micronesia', abbreviation: 'FM' },
-    { name: 'Florida', abbreviation: 'FL' },
-    { name: 'Georgia', abbreviation: 'GA' },
-    { name: 'Guam', abbreviation: 'GU' },
-    { name: 'Hawaii', abbreviation: 'HI' },
-    { name: 'Idaho', abbreviation: 'ID' },
-    { name: 'Illinois', abbreviation: 'IL' },
-    { name: 'Indiana', abbreviation: 'IN' },
-    { name: 'Iowa', abbreviation: 'IA' },
-    { name: 'Kansas', abbreviation: 'KS' },
-    { name: 'Kentucky', abbreviation: 'KY' },
-    { name: 'Louisiana', abbreviation: 'LA' },
-    { name: 'Maine', abbreviation: 'ME' },
-    { name: 'Marshall Islands', abbreviation: 'MH' },
-    { name: 'Maryland', abbreviation: 'MD' },
-    { name: 'Massachusetts', abbreviation: 'MA' },
-    { name: 'Michigan', abbreviation: 'MI' },
-    { name: 'Minnesota', abbreviation: 'MN' },
-    { name: 'Mississippi', abbreviation: 'MS' },
-    { name: 'Missouri', abbreviation: 'MO' },
-    { name: 'Montana', abbreviation: 'MT' },
-    { name: 'Nebraska', abbreviation: 'NE' },
-    { name: 'Nevada', abbreviation: 'NV' },
-    { name: 'New Hampshire', abbreviation: 'NH' },
-    { name: 'New Jersey', abbreviation: 'NJ' },
-    { name: 'New Mexico', abbreviation: 'NM' },
-    { name: 'New York', abbreviation: 'NY' },
-    { name: 'North Carolina', abbreviation: 'NC' },
-    { name: 'North Dakota', abbreviation: 'ND' },
-    { name: 'Northern Mariana Islands', abbreviation: 'MP' },
-    { name: 'Ohio', abbreviation: 'OH' },
-    { name: 'Oklahoma', abbreviation: 'OK' },
-    { name: 'Oregon', abbreviation: 'OR' },
-    { name: 'Palau', abbreviation: 'PW' },
-    { name: 'Pennsylvania', abbreviation: 'PA' },
-    { name: 'Puerto Rico', abbreviation: 'PR' },
-    { name: 'Rhode Island', abbreviation: 'RI' },
-    { name: 'South Carolina', abbreviation: 'SC' },
-    { name: 'South Dakota', abbreviation: 'SD' },
-    { name: 'Tennessee', abbreviation: 'TN' },
-    { name: 'Texas', abbreviation: 'TX' },
-    { name: 'Utah', abbreviation: 'UT' },
-    { name: 'Vermont', abbreviation: 'VT' },
-    { name: 'Virgin Islands', abbreviation: 'VI' },
-    { name: 'Virginia', abbreviation: 'VA' },
-    { name: 'Washington', abbreviation: 'WA' },
-    { name: 'West Virginia', abbreviation: 'WV' },
-    { name: 'Wisconsin', abbreviation: 'WI' },
-    { name: 'Wyoming', abbreviation: 'WY' },
-  ];
+  range(start: number, end: number, step = 1) {
+    const len = Math.floor((end - start) / step) + 1;
+    return Array(len)
+      .fill(null)
+      .map((_, idx) => start + idx * step);
+  }
 
   onSubmit(): void {
-    alert('Thanks!');
+    this.onSubmitEvent.emit(this.myReportForm.value);
   }
 }
